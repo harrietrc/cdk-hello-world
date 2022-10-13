32e813a0a477d34f6d79030e187b93d94fde2cb8
Harriet Robinson-Chen <harriet.robinson-chen@serato.com>
2022-10-13 17:58:14 +1300

    Add 'basic site' construct and hello world page

--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from 'aws-cdk-lib'
+import {SPADeploy} from 'cdk-spa-deploy'
 import { Construct } from 'constructs'
 
 export class CdkHelloWorldStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new SPADeploy(this, 'HelloWorldSite').createBasicSite({
+      indexDoc: 'index.html',
+      websiteFolder: 'public',
+    })
   }
 }
